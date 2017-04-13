409224e7d69e8289864c3446092fa5917b315162
Matthew Lavin <pbtflakes@protonmail.ch>
2017-04-13 13:41:57 -0400

    foo

--- theater.cpp
+++ theater.cpp
@@ -2,3 +2,29 @@
  * Theater group project
  * Period 5 C++ */
 #include <iostream>
+
+using namespace std;
+
+const int ROWS = 15, COLS = 30;
+const bool DEBUG = true;
+
+void	putseats(int[][COLS]);
+
+int main() {
+	char seatchart[ROWS][COLS];
+	int rowprice[ROWS];
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			seatchart[i][j] = '*';
+		}
+	}
+	
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			cout << seatchart[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
